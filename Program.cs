4b95e8a1d34478119b10a8c8747b94602d005227
songhh <525274895@qq.com>
2016-12-13 02:53:16 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using MySqlHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = "Server=localhost;UserId=root;Password=root;Database=test";
+            MySqlQueryHelper helper = new MySqlHelper.MySqlQueryHelper(conn);
+
+            var a = helper.Select("select * from log");
+            helper.CloseConnection();
+        }
+    }
+}
